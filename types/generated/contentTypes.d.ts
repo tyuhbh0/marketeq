a0e053e9dbf97c552311c05fa20169df5977261a
Manmohan Kumar <manmohankumar@Manmohans-MacBook-Pro.local>
2024-09-30 15:30:10 +0530

    update

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,36 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiUserEmailUserEmail extends Schema.CollectionType {
+  collectionName: 'user_emails';
+  info: {
+    singularName: 'user-email';
+    pluralName: 'user-emails';
+    displayName: 'User Email';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    User_Email: Attribute.Email;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::user-email.user-email',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::user-email.user-email',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -854,6 +884,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::user-email.user-email': ApiUserEmailUserEmail;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
