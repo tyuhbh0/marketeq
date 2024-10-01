302b7835db7122795cf320f33f368821e3ab2818
Manmohan Kumar <manmohankumar@Manmohans-MacBook-Pro.local>
2024-10-01 13:24:16 +0530

    update

---
+++ src/plugins/strapi-plugin-generate-schema.js
@@ -0,0 +1,11 @@
+// ./src/plugins/strapi-plugin-generate-schema.js
+
+module.exports = {
+    register(/* { strapi } */) {
+      // This is called before the application is initialized
+    },
+    bootstrap(/* { strapi } */) {
+      // This is called after the application is initialized
+    },
+  };
+  
